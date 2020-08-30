bd7d1c5dd3476dc3207ba9c0e1b3f77c1f0a17e2
takoyaki3 <mail@takoyaki3.com>
2020-08-30 17:06:28 +0900

    圧縮と取得を分離し、ゾンビプロセスになることを防止

--- volume/zip_files.php
+++ volume/zip_files.php
@@ -8,8 +8,11 @@ date_default_timezone_set('Asia/Tokyo');
 $counter=0;
 $last = time();
 
+$last_zip = date("Y_m_d_H");
+
 while(true){
 
+  $now_zip = date("Y_m_d_H");
   $is_zip = $last_zip!=$now_zip;
 
   // 読み込み用にtest.csvを開きます。
@@ -17,18 +20,16 @@ while(true){
 
   while($line = fgetcsv($f)){
     if( count($line) < 4) continue;
-    // ダウンロード対象でなければスキップ
-    if( $counter % (int)$line[0] != 0 ) continue;
 
-    // ディレクトリが存在していなければ作成
-    if(!file_exists('tmp/' . $line[1])) exec('mkdir ' . 'tmp/' . $line[1]);
+    // zip圧縮を行う
+    if($is_zip){
+      // ディレクトリが存在していなければ作成
+      if(!file_exists('data/' . $line[1])) exec('mkdir ' . 'data/' . $line[1]);
+      $cmd = 'nohup zip -j data/' . $line[1] .'/'. $last_zip.'.zip tmp/' . $line[1] .'/'.$last_zip.'_*; rm tmp/' . $line[1] .'/'.$last_zip.'_*.'.$line[3].' -f';
+      exec($cmd);
 
-    // URLをダウンロードして保存
-    $file = file_get_contents($line[2]);
-    $filename = date("Y_m_d_H_i_s") . '.' . $line[3];
-    file_put_contents( 'tmp/' . $line[1] . '/'. $filename,$file);
-
-    echo 'Get ' . $filename . ' ' . $line[2] . "\n";
+      echo 'Zip ' . $last_zip . ' ' . $line[1] . "\n";
+    }
   }
   // test.csvを閉じます。
   fclose($f);
